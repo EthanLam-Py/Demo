53fc43f90087a800deb250b9c891e6d7a8056387
EthanLam-Py <84464702+EthanLam-Py@users.noreply.github.com>
2021-06-06 00:48:18 +0700

    Add files via upload

---
+++ demo.py
@@ -0,0 +1,16 @@
+origin_list = [1,2,3,4,5,6,7,8,9,10, 'a', 'b', 'c']
+print('origin_list = ', origin_list)
+
+origin_list.reverse()
+print('reverse_list = ', origin_list)
+
+number_list = []
+char_list = []
+for item in origin_list:
+    if str(item).isdecimal():
+            number_list.append(item)
+    else:
+        char_list.append(item)
+
+print(number_list)
+print(char_list)
